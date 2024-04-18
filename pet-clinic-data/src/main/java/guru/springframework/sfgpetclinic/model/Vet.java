7390c08d3e069bece53100028e88869219569c7f
Rova2 <rova.rajoelisolo@bp2r.eu>
2024-04-18 12:49:46 +0400

    Add Vet List and update controller. Closes #15

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,4 +1,9 @@
 package guru.springframework.sfgpetclinic.model;
 
 public class Vet extends Person {
+    public String toString() {
+        return "{id='" + this.getId()
+                + "', firstName='" + this.getFirstName()
+                + "', lastName='" + this.getLastName() + "'}";
+    }
 }
