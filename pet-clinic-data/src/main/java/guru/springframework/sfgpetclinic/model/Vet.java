ecdd01d3f8c3ece480d746bd06c31b09a68faa6f
Rova2 <rova.rajoelisolo@bp2r.eu>
2024-04-19 10:40:30 +0400

    added specialties, added to vet. closes #27

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,6 +1,19 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
+
+    private Set<Specialty> specialties;
+
+    public Set<Specialty> getSpecialties() {
+        return specialties;
+    }
+
+    public void setSpecialties(Set<Specialty> specialties) {
+        this.specialties = specialties;
+    }
+
     public String toString() {
         return "{id='" + this.getId()
                 + "', firstName='" + this.getFirstName()
